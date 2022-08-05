cc01e9ec03ede0c4f19d2b3bc44c565fe823b22b
Samuel Wilson <sam.wilson@sharesight.co.nz>
2022-08-05 17:07:09 +1200

    Print out plain ascii art grid as a string
    This just shows a unlinked grid regardless of how the cells are linked.
    Displaying the links of a maze is the next step for the `to_string/1`
    function.

--- lib/twister/grid.ex
+++ lib/twister/grid.ex
@@ -15,6 +15,37 @@ defmodule Twister.Grid do
     }
   end
 
+  @doc """
+  to_string() returns a string representation of the grid as ASCII art.
+  """
+  def to_string(grid) do
+    top_boundary_string(grid.columns)
+    |> row_strings(grid.rows, grid.columns)
+    |> Enum.join()
+  end
+
+  defp top_boundary_string(columns) do
+    Enum.concat(
+      [
+        ["+"],
+        [Enum.map(1..columns, fn _ -> "---+" end)],
+        ["\n"]
+      ]
+    )
+  end
+
+  defp row_strings(acc, rows, columns) do
+    Enum.reduce(1..rows, acc, fn _, acc -> row_string(acc, columns) end)
+  end
+
+  defp row_string(acc, columns) do
+    acc
+    |> Enum.concat([ "|" | Enum.map(1..columns, fn _ -> "   |" end) ])
+    |> Enum.concat(["\n"])
+    |> Enum.concat([ "+" | Enum.map(1..columns, fn _ -> "---+" end) ])
+    |> Enum.concat(["\n"])
+  end
+
   defp build_cells(columns, rows) do
     coords = for column <- 1..columns, row <- 1..rows, do: {column, row}
 
