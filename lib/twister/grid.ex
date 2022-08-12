4756b18817922974a5ab0638ce631cb6f4b97b95
Samuel Wilson <sam.wilson@sharesight.co.nz>
2022-08-12 15:44:22 +1200

    Implement String.Chars protocol for Grid
    This allows the use of to_string/1 globally for getting the ascii art
    representation of a grid. This is also used by string interpolation.

--- lib/twister/grid.ex
+++ lib/twister/grid.ex
@@ -15,62 +15,61 @@ defmodule Twister.Grid do
     }
   end
 
-  @doc """
-  to_string() returns a string representation of the grid as ASCII art.
-  """
-  def to_string(grid) do
-    top_boundary_string(grid.columns)
-    |> row_strings(grid)
-    |> Enum.join()
-  end
-
-  defp top_boundary_string(columns) do
-    Enum.concat([
-      ["+"],
-      [Enum.map(1..columns, fn _ -> "---+" end)],
-      ["\n"]
-    ])
-  end
-
-  defp row_strings(acc, grid) do
-    Enum.reduce(1..grid.rows, acc, fn row, acc -> row_string(row, acc, grid) end)
-  end
-
-  defp row_string(row, acc, grid) do
-    acc
-    |> row_body_string(row, grid)
-    |> Enum.concat(["\n"])
-    |> row_bottom_boundary_string(row, grid)
-    |> Enum.concat(["\n"])
-  end
-
-  defp row_body_string(acc, row, grid) do
-    Enum.concat(acc, [
-      "|"
-      | Enum.map(1..grid.columns, fn column ->
-          case grid.cells[{column, row}].links do
-            [:east] -> "    "
-            [_] -> "   |"
-          end
-        end)
-    ])
-  end
-
-  defp row_bottom_boundary_string(acc, row, grid) do
-    Enum.concat(acc, [
-      "+"
-      | Enum.map(1..grid.columns, fn column ->
-          case grid.cells[{column, row}].links do
-            [:south] -> "   +"
-            [_] -> "---+"
-          end
-        end)
-    ])
-  end
-
   defp build_cells(columns, rows) do
     coords = for column <- 1..columns, row <- 1..rows, do: {column, row}
 
     Map.new(coords, fn {column, row} -> {{column, row}, Cell.new(column, row)} end)
+  end
+
+  defimpl String.Chars do
+    def to_string(grid) do
+      top_boundary_string(grid.columns)
+      |> row_strings(grid)
+      |> Enum.join()
+    end
+
+    defp top_boundary_string(columns) do
+      Enum.concat([
+        ["+"],
+        [Enum.map(1..columns, fn _ -> "---+" end)],
+        ["\n"]
+      ])
+    end
+
+    defp row_strings(acc, grid) do
+      Enum.reduce(1..grid.rows, acc, fn row, acc -> row_string(row, acc, grid) end)
+    end
+
+    defp row_string(row, acc, grid) do
+      acc
+      |> row_body_string(row, grid)
+      |> Enum.concat(["\n"])
+      |> row_bottom_boundary_string(row, grid)
+      |> Enum.concat(["\n"])
+    end
+
+    defp row_body_string(acc, row, grid) do
+      Enum.concat(acc, [
+        "|"
+        | Enum.map(1..grid.columns, fn column ->
+            case grid.cells[{column, row}].links do
+              [:east] -> "    "
+              _ -> "   |"
+            end
+          end)
+      ])
+    end
+
+    defp row_bottom_boundary_string(acc, row, grid) do
+      Enum.concat(acc, [
+        "+"
+        | Enum.map(1..grid.columns, fn column ->
+            case grid.cells[{column, row}].links do
+              [:south] -> "   +"
+              _ -> "---+"
+            end
+          end)
+      ])
+    end
   end
 end
